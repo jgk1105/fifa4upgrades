06f4c790fa484e64a23fff9a9f31af4d9c2f04bc
jgk1105 <48199036+jgk1105@users.noreply.github.com>
2021-12-07 20:22:31 +0900

    Add files via upload

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,14 +17,9 @@ from django.conf import settings
 from django.urls import include, path
 from django.conf.urls.static import static
 
-from django.views.static import serve
-from django.conf.urls import url
 
 urlpatterns = [
     path('', include('upgrades.urls', namespace="upgrades")),
-
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:
